3302173e368344ff3e9bff6029f07ddc8d3f2331
YoshiTikari <ricky.raths@gmail.com>
2015-06-17 16:31:03 +0200

    add php header

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <!--META VIEWPORT-->
-        <title>BLOCKSTACK</title>
-        <meta name="description" content="Ein interaktiver Wegweiser für die ersten Schritte mit Brackets.">
-        <script type="text/javascript" src="Libraries\matter-0.8.0.min.js"></script>
-        <script type="text/javascript" src="Libraries\jquery-1.11.3.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="Libraries\Foundation\css\foundation.min.css"></link>
-        <link rel="stylesheet" href="styles/custom.css"></link>
-    </head>
+    <?php
+        include 'header.html';
+    ?>
     <body>      
         <div id="menu">
             <h1>BLOCKSTACK</h1>
