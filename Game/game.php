760d532bca6ca5c41e291f0bc968de9d91ce7e76
Renshiro Takami <0toshirohitsugaya@gmail.com>
2015-06-18 13:10:33 +0200

    Game - added steady movement

--- Game/game.php
+++ Game/game.php
@@ -1,5 +1,6 @@
 
-<script type="text/javascript" src="../Libraries/matter.js"></script>  
+<script type="text/javascript" src="../Libraries/matter.js"></script>
+        
 <script>      
    // Initialize variables
    Width = window.innerWidth;
@@ -43,19 +44,28 @@
         lineWidth: 0,
       }
     });
-
+    
+    var angle = Math.PI / 2;
+    var maxAngle = Math.PI - Math.PI /5;
+    var minAngle = Math.PI /5;
+    var force = 0.01;
+    
    Events.on(engine, 'tick', function(event) {
-      /*for (t = 0; t < 2 * Math.PI; t += 0.01) {
-           Body.translate(elastic.bodyB, { x: x = 10 * Math.cos(t) + elastic.pointA.x, y: y = 10 * Math.sin(t) + elastic.pointA.y});
-        }*/
+       angle += force; 
+       
+       if(angle > maxAngle || angle < minAngle) {
+           force = -force;
+       }
+       
+       Body.setPosition(elastic.bodyB, { x: x = (Height/3 * Math.cos(angle)) + elastic.pointA.x, y: y = (Height/3 * Math.sin(angle)) + elastic.pointA.y});
     });        
 
    Events.on(engine, 'mousedown', function(event) {               
-       elastic.bodyB = Bodies.rectangle(Width / 2, Height / 3, Width /5, Width / 5, rockOptions);
+       elastic.bodyB = null;
     });           
 
     // add all of the bodies to the world
-    World.add(engine.world, [ground, mouseConstraint, rock, elastic]);           
+    World.add(engine.world, [ground, mouseConstraint, rock,  elastic]);           
 
     // run the engine
     Engine.run(engine)
