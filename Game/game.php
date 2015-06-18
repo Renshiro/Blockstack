b9b5683b4277ce57ba3581bf4d0273e86295e8cc
Renshiro Takami <0toshirohitsugaya@gmail.com>
2015-06-18 12:12:03 +0200

    Refactored ajax function and game

--- Game/game.php
+++ Game/game.php
@@ -1,71 +1,64 @@
-<!DOCTYPE html>
-    <head>
-        <meta charset="utf-8"/>
-        <script type="text/javascript" src="Libraries\matter-0.8.0.min.js"></script>
-        <script type="text/javascript" src="Libraries\jquery-1.11.3.min.js"></script>
-    </head>
-    <body>
-        
-       <script>      
-           // Initialize variables
-           Width = window.innerWidth;
-           Height = window.innerHeight;
-           
-           
-            // Matter.js module aliases
-            var Engine = Matter.Engine,
-            World = Matter.World,
-            Bodies = Matter.Bodies,
-            Body = Matter.Body
-            Constraint = Matter.Constraint,
-            Composites = Matter.Composites,
-            Events = Matter.Events,
-            MouseConstraint = Matter.MouseConstraint;
 
-            // create a Matter.js engine
-            var engine = Engine.create(document.body, {
-              render: {
-                options: {
-                  wireframes: false,
-                    width: Width,
-                    height: Height
-                }
-              }
-            });
+<script type="text/javascript" src="../Libraries/matter-0.8.0.min.js"></script>  
+<script>      
+   // Initialize variables
+   Width = window.innerWidth;
+   Height = window.innerHeight;
 
-            // add a mouse controlled constraint
-            var mouseConstraint = MouseConstraint.create(engine);
 
-            // create ground
-            var ground = Bodies.rectangle(Width / 2,Height,Width, Height / 10, { isStatic: true, render: { visible: true } }),
-            rockOptions = { density: 0.68, restitution: 1.0 },
-            rock = Bodies.rectangle(Width / 2, Height / 3, Width /5, Width / 5, rockOptions),
-            anchor = { x: Width / 2, y: 0 },
-            elastic = Constraint.create({
-              pointA: anchor,
-              bodyB: rock,
-              stiffness: 0.5,
-              render: {
-                lineWidth: 0,
-              }
-            });
-           
-           Events.on(engine, 'tick', function(event) {
-              for (t = 0; t < 2 * Math.PI; t += 0.01) {
-                   Body.setVelocity(elastic.bodyB, { x: x = 10 * Math.cos(t) + elastic.pointA.x, y: y = 10 * Math.sin(t) + elastic.pointA.y});
-                }
-            });        
-           
-           Events.on(engine, 'mousedown', function(event) {               
-               elastic.bodyB = Bodies.rectangle(Width / 2, Height / 3, Width /5, Width / 5, rockOptions);
-            });           
-           
-            // add all of the bodies to the world
-            World.add(engine.world, [ground, mouseConstraint, rock, elastic]);           
+    // Matter.js module aliases
+    var Engine = Matter.Engine,
+    World = Matter.World,
+    Bodies = Matter.Bodies,
+    Body = Matter.Body
+    Constraint = Matter.Constraint,
+    Composites = Matter.Composites,
+    Events = Matter.Events,
+    MouseConstraint = Matter.MouseConstraint;
 
-            // run the engine
-            Engine.run(engine)
-        </script>
-        
-    </body>
-</html>
+    // create a Matter.js engine
+    var engine = Engine.create(document.body, {
+      render: {
+        options: {
+          wireframes: false,
+            width: Width,
+            height: Height
+        }
+      }
+    });
+
+    // add a mouse controlled constraint
+    var mouseConstraint = MouseConstraint.create(engine);
+
+    // create ground
+    var ground = Bodies.rectangle(Width / 2,Height,Width, Height / 10, { isStatic: true, render: { visible: true } }),
+    rockOptions = { density: 0.68, restitution: 1.0 },
+    rock = Bodies.rectangle(Width / 2, Height / 3, Width /5, Width / 5, rockOptions),
+    anchor = { x: Width / 2, y: 0 },
+    elastic = Constraint.create({
+      pointA: anchor,
+      bodyB: rock,
+      stiffness: 0.5,
+      render: {
+        lineWidth: 0,
+      }
+    });
+
+    
+   var force = 2;
+    
+   Events.on(engine, 'tick', function(event) {
+       force = -force;
+       Body.translate(elastic.bodyB, {x: force, y: 0});
+    });        
+
+   Events.on(engine, 'mousedown', function(event) {               
+       elastic.bodyB = Bodies.rectangle(Width / 2, Height / 3, Width /5, Width / 5, rockOptions);
+    });           
+
+    // add all of the bodies to the world
+    World.add(engine.world, [ground, mouseConstraint, rock, elastic]);           
+
+    // run the engine
+    Engine.run(engine)
+</script>
--- Game/index.php
+++ Game/index.php
@@ -4,7 +4,7 @@
         include 'header.php';
     ?>
     <body>      
-        <div id="menu" style="height:100%">
+        <div id="menu" style="height:100%;">
             <div class="row" style="height:20%">
 				<div class="small-12 columns text-center"> 
 					<h1>Blockstack</h1> 
@@ -12,45 +12,35 @@
 			</div>
 			
 			<div class="row"  style="height:60%">
-				<div class="small-12 columns text-center"> <span id="Game" class="button round" onclick="loadGame()">Play Game</span> </div>
-				<div class="small-12 columns text-center"> <span id="How2Play" class="button round" onclick="loadHow2Play()">How To Play</span> </div>
-				<div class="small-12 columns text-center"> <span id="Highscores" class="button round" onclick="loadHighscores()">Highscores</span> </div>
-				<div class="small-12 columns text-center"> <span id="Credits" class="button round" onclick="loadCredits()">Credits</span> </div>
+				<div class="small-12 columns text-center"> <span id="Game" class="button round" onclick="loadPage('game.php', false)">Play Game</span> </div>
+				<div class="small-12 columns text-center"> <span id="How2Play" class="button round" onclick="loadPage('how2play.php', true)">How To Play</span> </div>
+				<div class="small-12 columns text-center"> <span id="Highscores" class="button round" onclick="loadPage('highscore.php', true)">Highscores</span> </div>
+				<div class="small-12 columns text-center"> <span id="Credits" class="button round" onclick="loadPage('Credits.php', true)">Credits</span> </div>
 			</div>        				
 			
         </div>
-        <div id="content" style="height:100%">
+        <div id="content">
             <!--contains code from other loaded HTML files-->
         </div>
     </body>
     <script>
-            function loadGame(){
-                $.ajax({url: "game.php", dataType:"html", success:
-                function(result){
-                    $("#menu").hide();
-					$("#content").html(result); //DA RICKY EFFEKT VERMUETLI UF RESULT
-				}});
-			};
-            function loadHow2Play(){
-            $.ajax({url: "how2play.php", dataType:"html", success:
+        function loadCredits(){
+            loadPage("Credits.php");
+        };
+        
+        function loadPage(site, bool){
+            $.ajax({url: site, dataType:"html", success:
                 function(result){
                     $("#menu").hide();
 					$("#content").html(result);
+                    if(bool){                        
+                        setContentHeight();
+                    }
 				}});
-			};
-            function loadHighscores(){
-            $.ajax({url: "highscore.php", dataType:"html", success:
-                function(result){
-                    $("#menu").hide();
-					$("#content").html(result);
-				}});
-			};
-            function loadCredits(){
-            $.ajax({url: "credits.php", dataType:"html", success:
-                function(result){
-                    $("#menu").hide();
-					$("#content").html(result);
-				}});
-			};
+        };
+        
+        function setContentHeight(){            
+            $("#content").height("100%");   
+        };
     </script>
 </html>
