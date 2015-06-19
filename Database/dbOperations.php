b6518cb2df177cc20e2e019ad58cd81300e85158
ROCKST4R <cross.sniper.CFS@gmail.com>
2015-06-19 11:28:07 +0200

    changed function in dbOperations.php
    added function

--- Database/dbOperations.php
+++ Database/dbOperations.php
@@ -18,4 +18,33 @@
 			$rank++;
 		}				
 	}
+
+    function setHighscore(var score, var name){
+        // Abfrage an Datenbank
+        $result = mysql_query("SELECT * FROM `highscore` Order by score Desc Limit 10;");
+        
+        $rank = 1;
+        
+        while ($row = mysql_fetch_object($result)) {
+            $bool = false;
+            if(score >= .$row->score.){
+                $bool = true;
+            }
+            $rank ++;
+        }
+        
+        if($bool) {
+            $query = "INSERT INTO highscore ('name', 'score') VALUES (NULL, $score)";
+        } else {
+            $$query = "SELECT 'id' FROM 'highscore' Where 'score' = $score";
+        }
+        
+        $result = mysql_query($query);
+        
+        //$result = mysql_query("INSERT INTO highscore ('name', 'score') VALUES (NULL, $score)");
+        
+        //$score_id = mysql_query("SELECT 'id' FROM 'highscore' Where 'score' = $score");
+        
+        //return score_id;
+    }
 ?>
